b30090b6984e82e8a586a3979c7a0360ef0c03a7
maximiliano.fried <maximiliano.fried@globant.com>
2020-02-09 10:44:31 +0100

    responsive not working

--- pages/simulacro.js
+++ pages/simulacro.js
@@ -2,11 +2,12 @@ import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 import withAuth from '../components/hoc/withAuth';
-const sampleText = `We and our partners use technologies, such as cookies, and process personal data, such as IP addresses and cookie identifiers,
+const SAMPLE_TEXT = `We and our partners use technologies, such as cookies, and process personal data, such as IP addresses and cookie identifiers,
 to personalise ads and content based on your interests, measure the performance of ads and content,
 and derive insights about the audiences who saw ads and content.
 Click below to consent to the use of this technology and the processing of your personal data for these purposes.
-You can change your mind and change your consent choices at any time by returning to this site.`
+You can change your mind and change your consent choices at any time by returning to this site.`;
+const EN_TEXT = "EN";
 import Tooltip from '../components/tooltip/tooltip';
 class Simulacro extends React.Component {
     render() {
@@ -15,30 +16,30 @@ class Simulacro extends React.Component {
                 <BasePage className="simulacro-page">
                 <div className="container-fluid testimonial-group">
                     <div className="row text-center flex-nowrap example">
-                        <div className="col-sm-4 title-lang">EN</div>
-                        <div className="col-sm-4 text-content">
+                        <div className="col-4 title-lang align-self-center">{EN_TEXT}</div>
+                        {/* <div className="col-4 text-content">
                         <Tooltip word="hover me!" pop="hello !!!!"/>
-                        </div>
-                        <div className="col-sm-4 text-content">{sampleText}</div>
-                        <div className="col-sm-4 text-content">{sampleText}</div>
-                        <div className="col-sm-4">5</div>
-                        <div className="col-sm-4">6</div>
-                        <div className="col-sm-4">7</div>
-                        <div className="col-sm-4">8</div>
-                        <div className="col-sm-4">9</div>
+                        </div> */}
+                        <div className="col-4 text-content align-self-center">{SAMPLE_TEXT}</div>
+                        <div className="col-4 text-content align-self-center">{SAMPLE_TEXT}</div>
+                        <div className="col-4 align-self-center"></div>
+                        <div className="col-4 align-self-center"></div>
+                        <div className="col-4 align-self-center"></div>
+                        {/*
+
+                        <div className="col-4"></div>
+                        <div className="col-4"></div> */}
                     </div>
-                </div>
-                <div className="container-fluid testimonial-group">
                     <div className="row text-center flex-nowrap example">
-                        <div className="col-sm-4 title-lang">ES</div>
-                        <div className="col-sm-4 text-content">{sampleText}</div>
-                        <div className="col-sm-4 text-content">{sampleText}</div>
-                        <div className="col-sm-4">4</div>
-                        <div className="col-sm-4">5</div>
-                        <div className="col-sm-4">6</div>
-                        <div className="col-sm-4">7</div>
-                        <div className="col-sm-4">8</div>
-                        <div className="col-sm-4">9</div>
+                        <div className="col-4 title-lang align-self-center">ES</div>
+                        <div className="col-4 text-content align-self-center">{SAMPLE_TEXT}</div>
+                        <div className="col-4 text-content align-self-center">{SAMPLE_TEXT}</div>
+                        <div className="col-4 align-self-center">{SAMPLE_TEXT}</div>
+                        <div className="col-4 align-self-center">{SAMPLE_TEXT}</div>
+                        <div className="col-4 align-self-center"></div>
+                        <div className="col-4 align-self-center"></div>
+                        <div className="col-4 align-self-center"></div>
+                        <div className="col-4 align-self-center"></div>
                     </div>
                 </div>
                 </BasePage>
