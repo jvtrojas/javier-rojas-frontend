5731670a665998e3037a9c6001b2d70dab7cea3a
maximiliano.fried <maximiliano.fried@globant.com>
2020-02-09 15:16:11 +0100

    best responsive aproach

--- pages/simulacro.js
+++ pages/simulacro.js
@@ -7,6 +7,8 @@ to personalise ads and content based on your interests, measure the performance 
 and derive insights about the audiences who saw ads and content.
 Click below to consent to the use of this technology and the processing of your personal data for these purposes.
 You can change your mind and change your consent choices at any time by returning to this site.`;
+const SAMPLE_TEXT_ES = `Nosotros y nuestros socios usamos tecnologías, como cookies, y procesamos datos personales, como direcciones IP e identificadores de cookies, para personalizar anuncios y contenido en función de sus intereses, medir el rendimiento de los anuncios y el contenido, y obtener información sobre el público que vio anuncios y contenido. Haga clic a continuación para consentir el uso de esta tecnología y el procesamiento de sus datos personales para estos fines. Puede cambiar de opinión y cambiar sus opciones de consentimiento en cualquier momento volviendo a este sitio.`
+const SAMPLE_TEXT_DE = `Wir und unsere Partner verwenden Technologien wie Cookies und verarbeiten personenbezogene Daten wie IP-Adressen und Cookie-IDs, um Anzeigen und Inhalte auf Grundlage Ihrer Interessen zu personalisieren, die Leistung von Anzeigen und Inhalten zu messen und Erkenntnisse über das Publikum zu gewinnen, das sie gesehen hat Anzeigen und Inhalte. Klicken Sie unten, um der Verwendung dieser Technologie und der Verarbeitung Ihrer persönlichen Daten für diese Zwecke zuzustimmen. Sie können Ihre Meinung und Ihre Einwilligungsentscheidungen jederzeit ändern, indem Sie auf diese Website zurückkehren.`
 const EN_TEXT = "EN";
 import Tooltip from '../components/tooltip/tooltip';
 class Simulacro extends React.Component {
@@ -15,14 +17,23 @@ class Simulacro extends React.Component {
             <BaseLayout {...this.props.auth} >
                 <BasePage className="simulacro-page">
                 <div className="container-fluid">
-                <div className="row wrap example ">
-<div className="col-lg-4 col-md-4 col-sm-4 col-4 title-lang">
-    <span className="title-lang">ES</span>
-</div>
-        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content">{SAMPLE_TEXT}</span> </div>
-        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content">{SAMPLE_TEXT}</span> </div>
+                    <div className="row wrap example">
+                        <div className="col-lg-4 col-md-4 col-sm-4 col-3 title-lang"><span className="title-lang">ES</span></div>
+                        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content">{SAMPLE_TEXT_ES}</span> </div>
+                        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content">{SAMPLE_TEXT_ES}</span> </div>
+                        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content"><Tooltip word="hover me !" pop="hello" /></span> </div>
 
-</div>
+                    </div>
+                    <div className="row wrap example">
+                        <div className="col-lg-4 col-md-4 col-sm-4 col-3 title-lang"><span className="title-lang">EN</span></div>
+                        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content">{SAMPLE_TEXT}</span> </div>
+                        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content">{SAMPLE_TEXT}</span> </div>
+                    </div>
+                    <div className="row wrap example">
+                        <div className="col-lg-4 col-md-4 col-sm-4 col-3 title-lang"><span className="title-lang">DE</span></div>
+                        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content">{SAMPLE_TEXT_DE}</span> </div>
+                        <div className="col-lg-6 col-md-8 col-sm-12 col-12"> <span className="text-content">{SAMPLE_TEXT_DE}</span> </div>
+                    </div>
                 </div>
                 </BasePage>
             </BaseLayout>
