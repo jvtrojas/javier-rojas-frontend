8118d6b8f737ab671d09b1109dd2f6b9fc38fdaf
Maxi <maximiliano.fried@gmail.com>
2020-02-20 08:17:51 +0100

    new title with svg component

---
+++ components/svg/background-grid.js
@@ -0,0 +1,244 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+import React, { useState } from 'react';
+
+const BackgroundGrid = (props) => {
+
+  return (
+    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+    width="1436.475px" height="266.33px" viewBox="0 0 1436.475 266.33" enable-background="new 0 0 1436.475 266.33">
+<g>
+   <g>
+       <g>
+           <g>
+ <line className="svg-background"  fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1436.475" y1="2.83" x2="1436.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1436.475" y1="20.83" x2="1436.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1436.475" y1="260.83" x2="1436.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1371.475" y1="2.83" x2="1371.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1371.475" y1="20.83" x2="1371.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1371.475" y1="260.83" x2="1371.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1305.475" y1="2.83" x2="1305.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1305.475" y1="20.83" x2="1305.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1305.475" y1="260.83" x2="1305.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1240.475" y1="2.83" x2="1240.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1240.475" y1="20.83" x2="1240.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1240.475" y1="260.83" x2="1240.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1175.475" y1="2.83" x2="1175.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1175.475" y1="20.83" x2="1175.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1175.475" y1="259.83" x2="1175.475" y2="265.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1110.475" y1="2.83" x2="1110.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1110.475" y1="20.83" x2="1110.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1110.475" y1="260.83" x2="1110.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1044.475" y1="2.83" x2="1044.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1044.475" y1="20.83" x2="1044.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1044.475" y1="260.83" x2="1044.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="979.475" y1="2.83" x2="979.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="979.475" y1="20.83" x2="979.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="979.475" y1="260.83" x2="979.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="914.475" y1="2.83" x2="914.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="914.475" y1="20.83" x2="914.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="914.475" y1="260.83" x2="914.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="848.475" y1="2.83" x2="848.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="848.475" y1="20.83" x2="848.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="848.475" y1="260.83" x2="848.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="783.475" y1="2.83" x2="783.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="783.475" y1="20.83" x2="783.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="783.475" y1="260.83" x2="783.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="718.475" y1="2.83" x2="718.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="718.475" y1="20.83" x2="718.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="718.475" y1="260.83" x2="718.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="652.475" y1="2.83" x2="652.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="652.475" y1="20.83" x2="652.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="652.475" y1="260.83" x2="652.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="587.475" y1="2.83" x2="587.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="587.475" y1="20.83" x2="587.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="587.475" y1="260.83" x2="587.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="522.475" y1="2.83" x2="522.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="522.475" y1="20.83" x2="522.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="522.475" y1="260.83" x2="522.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="457.475" y1="2.83" x2="457.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="457.475" y1="20.83" x2="457.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="457.475" y1="260.83" x2="457.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="391.475" y1="2.83" x2="391.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="391.475" y1="20.83" x2="391.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="391.475" y1="260.83" x2="391.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="326.475" y1="2.83" x2="326.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="326.475" y1="20.83" x2="326.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="326.475" y1="260.83" x2="326.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="261.475" y1="2.83" x2="261.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="261.475" y1="20.83" x2="261.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="261.475" y1="260.83" x2="261.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="195.475" y1="2.83" x2="195.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="195.475" y1="20.83" x2="195.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="195.475" y1="260.83" x2="195.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="130.475" y1="2.83" x2="130.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="130.475" y1="20.83" x2="130.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="130.475" y1="260.83" x2="130.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="65.475" y1="2.83" x2="65.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="65.475" y1="20.83" x2="65.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="65.475" y1="260.83" x2="65.475" y2="266.83"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="0.475" y1="2.83" x2="0.475" y2="8.83"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="0.475" y1="20.83" x2="0.475" y2="254.83"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="0.475" y1="260.83" x2="0.475" y2="266.83"/>
+           </g>
+       </g>
+   </g>
+   <g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1433.975" y1="266.33" x2="1427.975" y2="266.33"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1415.975" y1="266.33" x2="10.975" y2="266.33"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="4.975" y1="266.33" x2="-1.025" y2="266.33"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1433.975" y1="199.33" x2="1427.975" y2="199.33"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1415.975" y1="199.33" x2="10.975" y2="199.33"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="4.975" y1="199.33" x2="-1.025" y2="199.33"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1433.975" y1="133.33" x2="1427.975" y2="133.33"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1415.975" y1="133.33" x2="10.975" y2="133.33"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="4.975" y1="133.33" x2="-1.025" y2="133.33"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1433.975" y1="66.33" x2="1427.975" y2="66.33"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1415.975" y1="66.33" x2="10.975" y2="66.33"/>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="4.975" y1="66.33" x2="-1.025" y2="66.33"/>
+           </g>
+       </g>
+       <g>
+           <g>
+ <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="1433.975" y1="0.33" x2="1427.975" y2="0.33"/>
+
+     <line className="svg-background" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="12,12" x1="1415.975" y1="0.33" x2="10.975" y2="0.33"/>
+ <line fill="none" stroke="#0000F9" stroke-miterlimit="10" x1="4.975" y1="0.33" x2="-1.025" y2="0.33"/>
+           </g>
+       </g>
+   </g>
+</g>
+<rect x="280.475" y="939.33" fill="none" stroke="#0000F9" stroke-miterlimit="10" stroke-dasharray="15" width="3" height="6"/>
+</svg>
+  );
+}
+
+export default BackgroundGrid;
