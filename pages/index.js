d0d040306c9d8616810da9f7b0365db0f10b8464
Matias Espina <matiasespina1991@gmail.com>
2021-03-17 15:58:13 +0100

    css fixed - class names fixed - extra components deleted

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,6 @@ import BasePage from '../components/BasePage';
 import NavHeader from '../components/NavHeader'
 import Home from '../components/Home'
 import About from '../components/About'
-import LanguageComponent from '../components/subtitles/language';
-import EventTicket from '../components/event-ticket/eventTicket';
-import withAuth from '../components/hoc/withAuth';
-import Tooltip from '../components/tooltip/tooltip';
 
 const SAMPLE_TEXT = `AuralArk is the division of the creative platform simulacro focused on how space is perceived in sonic performances. We are in the search of futuristic spaces to experiment media events, a new hyperreal space created for the evolution of audiovisual and intermedia shows. By researching the aural architecture attributes of a venue, and relating to the will of the show, amount of people, and running hours we design specific architectural settings supported by high-end technology. `;
 const SAMPLE_TEXT_ES = `Nosotros y nuestros socios usamos tecnologías, como cookies, y procesamos datos personales, como direcciones IP e identificadores de cookies, para personalizar anuncios y contenido en función de sus intereses, medir el rendimiento de los anuncios y el contenido, y obtener información sobre el público que vio anuncios y contenido. Haga clic a continuación para consentir el uso de esta tecnología y el procesamiento de sus datos personales para estos fines. Puede cambiar de opinión y cambiar sus opciones de consentimiento en cualquier momento volviendo a este sitio.`
@@ -18,12 +14,10 @@ const EN_TEXT = "EN";
 class Index extends React.Component {
     render() {
         return (
-            <BaseLayout {...this.props.auth} >
-                <BasePage className="home-page">
-                    <NavHeader />
-                    <Home />
-                    <About />
-                </BasePage>
+            <BaseLayout className="global" {...this.props.auth} >
+                <NavHeader />
+                <Home className="home-page" />
+                <About className="about-page" />
             </BaseLayout>
         )
     }
