0455976ff55f085cbd2b6dd5f2532568bb795f49
Matias Espina <matiasespina1991@gmail.com>
2021-03-19 18:37:15 +0100

    hover color function for header ready - beta version

--- components/NavHeader.js
+++ components/NavHeader.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 function NavHeader() {
+
+    let colorsArray = ['#41FEFF','#C302FE','1034ef','#44FFC4','#FFF500','#F90F01'];
+    
+    function randomNumber() {
+        let number = Math.floor(Math.random() * colorsArray.length)
+        return number
+        }
+
+    const handleHoverColorChange = mouseEnter => {
+        let headerLink = mouseEnter.target;
+        headerLink.style.backgroundColor = colorsArray[randomNumber()]
+        headerLink.style.color = colorsArray[randomNumber()]
+    }
+
+    const handleDefaultColor = mouseLeave => {
+        let headerLink = mouseLeave.target;
+        headerLink.style.backgroundColor = 'white';
+        headerLink.style.color = 'black';
+        }
+
+
     return (
         <div className="container-fluid">
             {/* HEADER */}
@@ -12,17 +33,32 @@ function NavHeader() {
                 </div>
                 <div>
                     <div className="header-anchor aural-logo-header">
-                        <a href="#">ABOUT!!</a>          
+                        <a 
+                        href="#"
+                        onMouseEnter={mouseEnter => handleHoverColorChange(mouseEnter)} 
+                        onMouseLeave={mouseLeave => handleDefaultColor(mouseLeave)}
+                        >
+                        ABOUT!!</a>          
                     </div>
                 </div>
                 <div>
                     <div className="header-anchor aural-logo-header">
-                        <a href="#">PROJECTS!!</a>                       
+                        <a 
+                        href="#"
+                        onMouseEnter={mouseEnter => handleHoverColorChange(mouseEnter)} 
+                        onMouseLeave={mouseLeave => handleDefaultColor(mouseLeave)}
+                        >
+                        PROJECTS!!</a>                       
                     </div>
                 </div>
                 <div>
                     <div className="header-anchor aural-logo-header">
-                        <a href="#">AURAL_ARCHITECTURE!!</a>          
+                        <a 
+                        href="#"
+                        onMouseEnter={mouseEnter => handleHoverColorChange(mouseEnter)} 
+                        onMouseLeave={mouseLeave => handleDefaultColor(mouseLeave)}
+                        >
+                        AURAL_ARCHITECTURE!!</a>          
                     </div>
                 </div>
                     <div>
