7f29aaa1b4f918ba06bec612dce3adedda847330
Matias Espina <matiasespina1991@gmail.com>
2021-03-22 09:53:12 +0100

    minor fixes

--- pages/about.js
+++ pages/about.js
@@ -14,83 +14,16 @@ class About extends React.Component {
 
     return (
         <div className="no-padding">
-                    
-        
-                    {/* Row lang EN */}
-                    
-                    <div className="row-about wrap hide-scrollbar webkit-scrollbar">
-                        
-                             <div className="selected-class black-block title_projects">
-                             EN
-
-                            
-                            </div>
-                         <div><img className="imgrow" src="static/images/9_16/4A9C9416-477D-4496-96B4-088EDC1D74AB.jpg" alt="ATERNATIVO"/>
-                        
-                        
-                            </div>
-
-
-                        
-
-                        
-                         
-                    
-                    
-             </div>
-                        
-                    
-
-              <div className="container-fluid about-page">
-            
-            {/* Row 1 EMPTY */}
-            <div className="row row-no_top_padding wrap hide-scrollbar">
-                About
-                <div className="border-block">
-                    1
+            {/* Row lang EN */}
+            <div className="row-about wrap hide-scrollbar webkit-scrollbar">
+                <div className="selected-class black-block title_projects">
+                EN
                 </div>
-                <div className="border-block">
-                    2
-                </div>
-                <div className="border-block">
-                    3               
-                </div>
-                <div className="border-block">
-                    4 
+                <div>
+                    <img className="imgrow" src="static/images/9_16/4A9C9416-477D-4496-96B4-088EDC1D74AB.jpg" alt="ATERNATIVO"/>
                 </div>
             </div>
-
-            {/* Row 2 */}
-            <div className="row wrap hide-scrollbar">
-                <div className="border-block">
-                a
-                </div>
-                <div className="border-block">
-                b
-                </div>
-                <div className="border-block">
-                c
-                </div>
-                <div className="border-block">
-                d
-                </div>
-                <div className="border-block">
-                e
-                </div>
-            </div>
-
-            {/* Row 3 */}
-            <div className="row wrap hide-scrollbar">
-                <div className="border-block">
-
-                </div>
-                <div className="border-block">
-                
-                </div>
-                <div className="border-block">
-                </div>
-            
-           
+        </div>
     )
 }
 }
