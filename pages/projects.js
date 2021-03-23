2391101bfc691a8ac13a5372a2c53f39dbd4ab1a
Matias Espina <matiasespina1991@gmail.com>
2021-03-23 10:58:09 +0100

    minor css fixes

--- pages/projects.js
+++ pages/projects.js
@@ -3,21 +3,23 @@ import Data from '../public/js/data-home.json';
 const data = Data.map(each => {return each})
 const row1 = [];
 const row2 = [];
+const row3 = [];
 
 data.forEach(object => object.row==1 ? row1.push(object) : "")
 data.forEach(object => object.row==2 ? row2.push(object) : "")
+data.forEach(object => object.row==3 ? row3.push(object) : "")
 
 function Projects() {
     return(
         <div className="container-fluid">
             {/* Row 1 */}
-            <div className="row row-no_top_padding wrap hide-scrollbar">
+            <div className="wrap row row-no_top_padding row-slide-wrapper hide-scrollbar">
                 { 
                     row1.map(
                         image => {
                             return(
-                                    <div className="border-block">
-                                        <img src={ image.src } alt={ image.title } />
+                                    <div className="row-pics-container">
+                                        <img className="row-pics" src={ image.src } alt={ image.title } />
                                     </div>
                             )
                         }
@@ -26,19 +28,35 @@ function Projects() {
             </div>
 
             {/* Row 2 */}
-            <div className="row row-no_top_padding wrap hide-scrollbar">
+            <div className="wrap row row-no_top_padding row-slide-wrapper hide-scrollbar">
                 { 
                     row2.map(
                         image => {
                             return(
-                                    <div className="border-block">
-                                        <img src={ image.src } alt={ image.title } />
+                                    <div className="row-pics-container">
+                                        <img className="row-pics" src={ image.src } alt={ image.title } />
                                     </div>
                             )
                         }
                     )
                 }
             </div>
+
+            {/* Row 3 */}
+            <div className="wrap row row-no_top_padding row-slide-wrapper hide-scrollbar">
+                { 
+                    row3.map(
+                        image => {
+                            return(
+                                    <div className="row-pics-container">
+                                        <img className="row-pics" src={ image.src } alt={ image.title } />
+                                    </div>
+                            )
+                        }
+                    )
+                }
+            </div>
+
         </div>
     )    
 }
