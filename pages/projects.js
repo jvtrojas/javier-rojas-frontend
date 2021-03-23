0e36cf59cd9f9369072e9df4cc60b047e3f1f686
Matias Espina <matiasespina1991@gmail.com>
2021-03-23 18:09:14 +0100

    youtube support on all rows

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,4 @@
 import Data from '../public/js/data-home.json';
-import BlackSquareShuffler from '../components/layouts/BlackSquareShuffler';
 
 const data = Data.map(each => {return each})
 const row1 = [];
@@ -15,12 +14,16 @@ const shuffleOrder = () => {
     return( { order: `${randomNumber}` } )
 }
 
+const handleClick = () => {
+
+}
+
 function Projects() {
 
     return(
         <div className="container-fluid">
             {/* Row 1 */}
-            <div className="wrap row row-no_top_padding row-slide-wrapper hide-scrollbar">
+            <div onClick={handleClick} className="wrap row row-no_top_padding row-slide-wrapper hide-scrollbar">
                 { 
                     row1.map(
                         function(image) {
@@ -47,14 +50,25 @@ function Projects() {
 
             {/* Row 2 */}
             <div className="wrap row row-no_top_padding row-slide-wrapper hide-scrollbar">
-                { 
-                    row2.map(
-                        image => {
-                            return(
-                                    <div className="row-pics-container" style={shuffleOrder()} >
-                                        <img className="row-pics" src={ image.src } alt={ image.title } />
-                                    </div>
-                            )
+            { 
+                    row1.map(
+                        function(image) {
+                            if(image.src.length!==11){
+                                return(
+                                        <div className="row-pics-container" style={shuffleOrder()} >
+                                            <img className="row-pics" src={ image.src } alt={ image.title } />
+                                        </div>
+                                )
+                            } else {
+                                    return(
+                                            <div className="row-pics-container youtube-link" style={shuffleOrder()} >
+                                                <iframe width="auto" height="100%" 
+                                                    src={`https://www.youtube.com/embed/${image.src}`} title="YouTube video player" 
+                                                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+                                                />
+                                            </div>
+                                    )
+                            }
                         }
                     )
                 }
@@ -62,19 +76,29 @@ function Projects() {
 
             {/* Row 3 */}
             <div className="wrap row row-no_top_padding row-slide-wrapper hide-scrollbar">
-                { 
-                    row3.map(
-                        image => {
-                            return(
-                                    <div className="row-pics-container" style={shuffleOrder()} >
-                                        <img className="row-pics" src={ image.src } alt={ image.title } />
-                                    </div>
-                            )
+            { 
+                    row1.map(
+                        function(image) {
+                            if(image.src.length!==11){
+                                return(
+                                        <div className="row-pics-container" style={shuffleOrder()} >
+                                            <img className="row-pics" src={ image.src } alt={ image.title } />
+                                        </div>
+                                )
+                            } else {
+                                    return(
+                                            <div className="row-pics-container youtube-link" style={shuffleOrder()} >
+                                                <iframe width="auto" height="100%" 
+                                                    src={`https://www.youtube.com/embed/${image.src}`} title="YouTube video player" 
+                                                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+                                                />
+                                            </div>
+                                    )
+                            }
                         }
                     )
                 }
             </div>
-
         </div>
     )    
 }
