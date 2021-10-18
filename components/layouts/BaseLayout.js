18b7fb6abd94169ee095ab99af18fa6f9d68ce80
jvtrojas <javito.rojas@gmail.com>
2021-10-18 17:10:20 +0200

    font links fixed

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -11,10 +11,10 @@ const BaseLayout = (props) => {
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
                 <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;700&display=swap" rel="stylesheet"/>
-                <link href="https://www.allfont.es/cache/fonts/bpdotscondensedsquare_94cd44a67704432b013aa6c9ed99c81e.ttf" rel="stylesheet" type="text/css" />
+                <link rel="preload" href="https://www.allfont.es/cache/fonts/bpdotscondensedsquare_94cd44a67704432b013aa6c9ed99c81e.ttf" as="font" crossorigin="anonymous" />
                 <link rel="preload" href="https://ggayane.github.io/css-experiments/Inconsolata-VF.ttf" as="font" crossorigin="anonymous" />
-                <link rel="preload" href="https://res.cloudinary.com/hzisufglx/raw/upload/v1626072237/B_Pdots_Condensed_Diamond_2f853c24bf.ttf" rel="stylesheet" type="text/css" />
-                <link rel="preload" href="https://res.cloudinary.com/hzisufglx/raw/upload/v1626072589/B_Pdots_Condensed_619d51fb3c.ttf" rel="stylesheet" type="text/css" />
+                <link rel="preload" href="https://res.cloudinary.com/hzisufglx/raw/upload/v1626072237/B_Pdots_Condensed_Diamond_2f853c24bf.ttf" as="font" crossorigin="anonymous" />
+                <link rel="preload" href="https://res.cloudinary.com/hzisufglx/raw/upload/v1626072589/B_Pdots_Condensed_619d51fb3c.ttf" as="font" crossorigin="anonymous"/>
             </Head>
             <div>
                 {children}
