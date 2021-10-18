7680d583f608c3d746a95f04491a5524a878c9cf
jvtrojas <javito.rojas@gmail.com>
2021-10-18 13:58:14 +0200

    font links fixed

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -8,6 +8,13 @@ const BaseLayout = (props) => {
             <Head>
                 <title>Javier-Rojas.xyz</title>
                 <link rel="icon" type="image/ico" href="/static/favicon.ico"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+                <link href="https://allfont.es/allfont.css?fonts=bpdotscondensedsquare" rel="stylesheet" type="text/css" />
+                <link rel="preload" href="https://ggayane.github.io/css-experiments/Inconsolata-VF.ttf" as="font" crossorigin="anonymous" />
+                <link href="https://allfont.es/allfont.css?fonts=bpdotscondenseddiamond" rel="stylesheet" type="text/css" />
+                <link href="https://allfont.es/allfont.css?fonts=bpdotscondensed" rel="stylesheet" type="text/css" />
             </Head>
             <div>
                 {children}
